f6c81a48ea79b353c6eead7a0fc6078d70fb60b6
yurec0098 <yurec0098@rambler.ru>
2021-10-14 19:54:32 +0300

    Lesson_2_2

--- Lesson_2/BankAccount.cs
+++ Lesson_2/BankAccount.cs
@@ -10,18 +10,24 @@ namespace Lesson_2
 		Payments
 	};
 
-	/*1. Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип банковского счета (использовать перечислимый тип).
-	 * Предусмотреть методы для доступа к данным – заполнения и чтения.
-	 * Создать объект класса, заполнить его поля и вывести информацию об объекте класса на печать.*/
+	/*
+	1. Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип банковского счета (использовать перечислимый тип).
+	Предусмотреть методы для доступа к данным – заполнения и чтения.
+	Создать объект класса, заполнить его поля и вывести информацию об объекте класса на печать.
+	 
+	2. Изменить класс счет в банке из упражнения таким образом, чтобы номер счета генерировался сам и был уникальным.
+	Для этого надо создать в классе статическую переменную и метод, который увеличивает значение этого переменной.*/
 	public class BankAccount
 	{
 		public override string ToString() => $"[{ID}] ({Type}) {Balance}";
 
-		string _id;
+		static decimal LastID = 0;
+
+		decimal _id;
 		decimal _balance;
 		AccountType _type;
 
-		public string ID => _id;    //	Номер счёта не должен меняться
+		public decimal ID => _id;    //	Номер счёта не должен меняться
 		public decimal Balance
 		{
 			get => _balance;
@@ -35,7 +41,7 @@ namespace Lesson_2
 
 		public BankAccount(AccountType type)
 		{
-			_id = $"{Guid.NewGuid()}";
+			_id = NewID();
 			Type = type;
 		}
 		public BankAccount(AccountType type, decimal balance) : this(type)
@@ -46,6 +52,11 @@ namespace Lesson_2
 		public string Print()
 		{
 			return $"Account: {ID}{Environment.NewLine}Acoount type: {Type}{Environment.NewLine}Balance: {Balance}";
+		}
+
+		private decimal NewID()
+		{
+			return ++LastID;
 		}
 	}
 }
