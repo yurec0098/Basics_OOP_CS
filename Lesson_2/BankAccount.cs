78f6cc13b4216390aaff7aa7b96720000ea10c1b
yurec0098 <yurec0098@rambler.ru>
2021-10-14 19:38:28 +0300

    Добавьте файлы проекта.

---
+++ Lesson_2/BankAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Lesson_2
+{
+	public enum AccountType
+	{
+		Current,
+		Credit,
+		Deposit,
+		Payments
+	};
+
+	/*1. Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип банковского счета (использовать перечислимый тип).
+	 * Предусмотреть методы для доступа к данным – заполнения и чтения.
+	 * Создать объект класса, заполнить его поля и вывести информацию об объекте класса на печать.*/
+	public class BankAccount
+	{
+		public override string ToString() => $"[{ID}] ({Type}) {Balance}";
+
+		string _id;
+		decimal _balance;
+		AccountType _type;
+
+		public string ID => _id;	//	Номер счёта не должен меняться
+		public decimal Balance
+		{
+			get => _balance;
+			set => _balance = value;
+		}
+		public AccountType Type
+		{
+			get => _type;
+			set => _type = value;
+		}
+
+		public BankAccount(string id, AccountType type)
+		{
+			_id = id;
+			Type = type;
+		}
+		public BankAccount(string id, AccountType type, decimal balance)
+		{
+			_id = id;
+			Type = type;
+			Balance = balance;
+		}
+
+		public string Print()
+		{
+			return $"Account: {ID}{Environment.NewLine}Acoount type: {Type}{Environment.NewLine}Balance: {Balance}";
+		}
+	}
+}
---
+++ Lesson_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var bankAccount = new BankAccount($"{Guid.NewGuid()}", AccountType.Current, 250);
+			Console.WriteLine(bankAccount.Print());
+		}
+	}
+}
