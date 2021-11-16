e42e2f71475a4b308fb8bef9c0b8271bb1223572
yurec0098 <yurec0098@rambler.ru>
2021-11-16 18:55:00 +0200

    Lesson_4_2

---
+++ Lesson_4/Creator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Lesson_4
+{
+	/*
+	2. * Для реализованного класса создать новый класс Creator, который будет являться фабрикой объектов класса здания.
+	Для этого изменить модификатор доступа к конструкторам класса, в новый созданный класс добавить перегруженные фабричные методы CreateBuild для создания объектов класса здания.
+	В классе Creator все методы сделать статическими, конструктор класса сделать закрытым.
+	Для хранения объектов класса здания в классе Creator использовать хеш-таблицу.
+	Предусмотреть возможность удаления объекта здания по его уникальному номеру из хеш-таблицы.
+	Создать тестовый пример, работающий с созданными классами.
+	 */
+	public class Creator : Build
+	{
+		public static Hashtable Builds = new Hashtable();
+
+		private Creator() { }
+
+		public static Build CreateBuild()
+		{
+			Build build = new Creator();
+			Builds.Add(build.GetID() ,build);
+			return build;
+		}
+
+		public static void DeleteBuild(int id)
+		{
+			if(Builds.ContainsKey(id))
+				Builds.Remove(id);
+		}
+	}
+}
--- Lesson_4/Program.cs
+++ Lesson_4/Program.cs
@@ -7,7 +7,20 @@ namespace Lesson_4
 	{
 		static void Main(string[] args)
 		{
+			var random = new Random();
+			for (int i = 0; i < 10; i++)
+				Creator.CreateBuild();
 
+			foreach (Build build in Creator.Builds.Values)
+				Console.WriteLine($"Build ID: {build.GetID()}");
+
+			Console.WriteLine();
+			var index = random.Next(1, 10);
+			Console.WriteLine($"Delete build ID: {index}");
+			Creator.DeleteBuild(index);
+
+			foreach (Build build in Creator.Builds.Values)
+				Console.WriteLine($"Build ID: {build.GetID()}");
 		}
 	}
 }
